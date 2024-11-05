86780273c944769644a99b54fb285c52649d8956
AlexDolz <aleksejdolznikov3@gmail.com>
2024-11-05 17:18:23 +0100

    handling hover effects

--- use_popcorn/src/components/StarRating.jsx
+++ use_popcorn/src/components/StarRating.jsx
@@ -17,7 +17,8 @@ const textStyle = {
 };
 
 function StarRating({ maxRating = 5 }) {
-  const [rating, setRating] = useState(1);
+  const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
@@ -28,12 +29,14 @@ function StarRating({ maxRating = 5 }) {
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
             key={i}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
             onRate={() => handleRating(i + 1)}
-            full={rating >= i + 1}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
           />
         ))}
       </div>
-      <p style={textStyle}>{rating || ""}</p>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   );
 }
