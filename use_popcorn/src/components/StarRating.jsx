8f2eaeedb0bc72e0124f17c2d579c0b169a5be0f
AlexDolz <aleksejdolznikov3@gmail.com>
2024-11-06 17:10:41 +0100

    propTypes

--- use_popcorn/src/components/StarRating.jsx
+++ use_popcorn/src/components/StarRating.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Star from "./Star";
+import PropTypes from "prop-types";
 
 const containerStyle = {
   display: "flex",
@@ -9,6 +10,16 @@ const containerStyle = {
 
 const starContainerStyle = {
   display: "flex",
+};
+
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  messages: PropTypes.array,
+  className: PropTypes.string,
+  onSetRating: PropTypes.func,
 };
 
 function StarRating({
