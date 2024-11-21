0f088240dda94a1735e1e679dba5dd4a3bcd851e
AlexDolz <aleksejdolznikov3@gmail.com>
2024-11-21 16:46:54 +0100

    cleaning up data fetching

--- use_popcorn/src/App.js
+++ use_popcorn/src/App.js
@@ -40,13 +40,17 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
-          const res =
-            await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=${query}
-      `);
+          const res = await fetch(
+            `http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=${query}
+      `,
+            { signal: controller.signal }
+          );
 
           if (!res.ok)
             throw new Error("Something went wrong with fetching movies");
@@ -54,8 +58,11 @@ export default function App() {
           const data = await res.json();
           if (data.Response === "False") throw new Error("Movie not found");
           setMovies(data.Search);
+          setError("");
         } catch (err) {
-          setError(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -70,6 +77,10 @@ export default function App() {
       }
 
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
